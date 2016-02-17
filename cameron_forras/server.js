9f24b788fd5898d79550b3c32023fa2785239d1e
Cameron Forras <cameronforras@hotmail.com>
2016-02-16 23:07:33 -0800

    fixed auth routes

--- cameron_forras/server.js
+++ cameron_forras/server.js
@@ -7,9 +7,11 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/students_app_d
 
 const studentsRouter = require(__dirname + '/routes/students_routes');
 const classesRouter = require(__dirname + '/routes/classes_routes');
+const authRouter = require(__dirname + '/routes/auth_routes');
 
 app.use('/api', studentsRouter);
 app.use('/api', classesRouter);
+app.use(authRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Yo! Server up on port: ' + PORT));
