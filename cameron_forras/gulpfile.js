1826f6ce5c9252df5786266294efdecaee61a490
Cameron Forras <cameronforras@hotmail.com>
2016-02-16 23:42:10 -0800

    working, just need to add other resource

--- cameron_forras/gulpfile.js
+++ cameron_forras/gulpfile.js
@@ -1,38 +1,21 @@
-'use strict';
-const gulp = require('gulp'),
-      eslint = require('gulp-eslint'),
-      mocha = require('gulp-mocha'),
-      files = ['test/*.js', '!node_modules//**'];
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const files = ['test/*.js', '!node_modules//**'];
 
-gulp.task('lint', function() {
-  return gulp.src(files)
-    .pipe(eslint({
-      extends: 'eslint:recommended', // imports general rules
-      ecmaFeatures: {
-        'modules': true,       // allows modules
-        'blockBindings': true,  // allows const
-        'arrowFunctions': true  // allows arrow functions
-      },
-      'rules': {
-        'no-console': 0,       // allows console.logs without throwing err
-        'semi': 2,             // requires semi-colons
-        'no-unused-vars': 0
-      },
-      envs: [
-        'node',
-        'mocha'
-      ]
+gulp.task('html:dev', () => {
+  gulp.src(__dirname + '/app/**/*.html')
+    .pipe(gulp.dest(__dirname + '/build'));
+});
+
+gulp.task('webpack:dev', () => {
+  gulp.src(__dirname + '/app/js/client.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
     }))
-    .pipe(eslint.format());
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('mocha', function() {
-  return gulp.src(['test/*.js'], { read: false })
-    .pipe(mocha());
-});
-
-gulp.task('watch', function() {
-  gulp.watch(files, ['lint']);
-});
-
-gulp.task('default', ['mocha', 'lint', 'watch']);
+gulp.task('build:dev', ['webpack:dev', 'html:dev']);
+gulp.task('default', ['build:dev']);
--- cameron_forras/server.js
+++ cameron_forras/server.js
@@ -2,16 +2,20 @@
 const express = require('express');
 const app = module.exports = exports = express();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/students_app_dev');
 
 const studentsRouter = require(__dirname + '/routes/students_routes');
 const classesRouter = require(__dirname + '/routes/classes_routes');
-const authRouter = require(__dirname + '/routes/auth_routes');
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
 app.use('/api', studentsRouter);
 app.use('/api', classesRouter);
-app.use(authRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Yo! Server up on port: ' + PORT));
